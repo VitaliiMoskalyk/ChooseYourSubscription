d2eee1b626802f9693ee27f542e402f12b0037c8
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2022-06-30 22:52:01 +0300

    fix header svg

--- src/views/Header/Header.jsx
+++ src/views/Header/Header.jsx
@@ -5,18 +5,18 @@ import { useWindowWidth } from '@react-hook/window-size';
 
 const Header = ({ opener, svgStatus }) => {
   const onlyWidth = useWindowWidth();
-  const newLocal = '#';
+
   return (
     <Head>
       <img src={MainLogo} alt="MainLogo" width="32" />
       {onlyWidth < 767 && (
-        <a href={newLocal} onClick={opener}>
+        <div onClick={opener}>
           {svgStatus ? (
             <Svg width="28" height="28" icon="icon-CloseModal" />
           ) : (
             <Svg width="18" height="16" icon="icon-Vector" />
           )}
-        </a>
+        </div>
       )}
     </Head>
   );
